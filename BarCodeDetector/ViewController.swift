92cd2ed512b9f56597cb4a06d16e79789507d1b5
Edilson Ngulele <edilsonngulele@gmail.com>
2018-07-11 23:35:30 +0200

    Initial Commit

---
+++ BarCodeDetector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BarCodeDetector
+//
+//  Created by Edilson Ngulele on 7/11/18.
+//  Copyright © 2018 Edilson Ngulele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
